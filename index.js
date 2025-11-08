0b96d3adc846515f1644d0443328a3e3fc726c62
Anirban Mukherjee <anirbanmukherjee.cse@gmailcom>
2025-11-09 05:35:10 +0600

    made apis for various data fetching

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
-
+require("dotenv").config();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -11,9 +12,7 @@ app.use(express.json());
 
 // mongodb connection string
 
-const uri =
-  "mongodb+srv://smartdbUser:smartdb1001@cluster0.lpak3ak.mongodb.net/next_db?appName=Cluster0";
-
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lpak3ak.mongodb.net/next_db?appName=Cluster0`;
 // mongo client
 const client = new MongoClient(uri, {
   serverApi: {
@@ -55,25 +54,66 @@ async function run() {
       res.json(result);
     });
 
-    //   ---------------------------------course apis----------------------------
+    //   ---------------------------------courses apis----------------------------
 
-    //   get all courses
+    //   get all courses api
     app.get("/courses", async (req, res) => {
       const result = await coursesCollection.find().toArray();
       res.json(result);
     });
 
-    //   add a course
+    //   get course by id api
+    app.get("/courses/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await coursesCollection.findOne(query);
+      res.json(result);
+    });
 
+    //   add a course
     app.post("/courses", async (req, res) => {
       const newCourse = req.body;
       const result = await coursesCollection.insertOne(newCourse);
       res.json(result);
     });
-      
-      //   edit a course
-      app.patch
 
+    //   update a course
+
+    app.put("/courses/:id", async (req, res) => {
+      const updatedCourse = req.body;
+      const result = await coursesCollection.updateOne(
+        {
+          _id: new ObjectId(req.params.id),
+        },
+        {
+          $set: updatedCourse,
+        }
+      );
+      res.json(result);
+    });
+
+    //   delete a course by id api
+    app.delete("/courses/:id", async (req, res) => {
+      const query = { _id: new ObjectId(req.params.id) };
+      const result = await coursesCollection.deleteOne(query);
+      res.json(result);
+    });
+
+    //   -------------------------------enrollment apis---------
+
+    //   user enrolls in a course(post enroll)
+    app.post("/enroll", async (req, res) => {
+      const newEnrollment = req.body;
+      const result = await enrollmentsCollection.insertOne(newEnrollment);
+      res.json(result);
+    });
+
+    //   get enrolled courses for a user
+    app.get("/enrollments/:userId", async (req, res) => {
+      const querry = { userId: req.params.userId };
+      const result = await enrollmentsCollection.find(querry).toArray();
+      res.json(result);
+    });
     // Send a ping to confirm a successful connection
   } finally {
     // Ensures that the client will close when you finish/error
--- src/config/db.js
+++ src/config/db.js
@@ -1,4 +1,29 @@
-const { MongoClient, ServerApiVersion } = require("mongodb");
+import { MongoClient, ServerApiVersion } from "mongodb";
+
+
+
 const connectDB = async () => {
+
+    // uri
     
+    // mongo client
+    const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    },
+    });
+    
+    
+    try {
+        await client.connect();
+        console.log("Mongo connected")
+
+    }
+    catch (error) {
+        console.log("Mongo error", error)
+    }
 }
+
+export default connectDB;
